0135deef6f17ce71123fac740db005b4b0846a23
Luis Parrado <luisprmat@gmail.com>
2025-03-17 22:10:32 -0500

    Shadcn Table: Tasks List with TypeScript

--- resources/js/pages/Tasks/Index.tsx
+++ resources/js/pages/Tasks/Index.tsx
@@ -1,7 +1,9 @@
 import AppLayout from '@/layouts/app-layout';
-import { type BreadcrumbItem } from '@/types';
+import { Task, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,13 +12,34 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Index() {
+export default function Index({ tasks }: { tasks: Task[] }) {
     const { t } = useLaravelReactI18n();
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={t(':Items List', { items: t('tasks') })} />
-            <div>The list will be here.</div>
+            <div>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>{t('Task')}</TableHead>
+                            <TableHead className="w-[100px]">{t('Status')}</TableHead>
+                            <TableHead className="w-[150px] text-right">{t('Actions')}</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {tasks.map((task) => (
+                            <TableRow key={task.id}>
+                                <TableCell>{task.name}</TableCell>
+                                <TableCell className={task.is_completed ? 'text-green-600' : 'text-red-700'}>
+                                    {task.is_completed ? t('Completed') : t('In Progress')}
+                                </TableCell>
+                                <TableCell className="text-right"></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </AppLayout>
     );
 }
